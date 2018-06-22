231c8ed7d891bc9d3b9c17da210aa3d9e374c38f
Peter Hanula <miko3k@users.noreply.github.com>
2018-06-22 22:49:25 +0200

    some javadoc

--- hardcode-annotations/src/main/java/org/deletethis/hardcode/HardcodeRoot.java
+++ hardcode-annotations/src/main/java/org/deletethis/hardcode/HardcodeRoot.java
@@ -5,6 +5,14 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks something, that should be created by a separate method.
+ *
+ * <p>It is used to split object graph to smaller parts in order to keep methods under 64kb limit. May be applied
+ * to classes, or to fields. Any object, that is marked as {@code @HardcodeRoot}, must be referenced exactly once.
+ *
+ * <p>Can be applied on fields or classes.</p>
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD})
 public @interface HardcodeRoot {
--- hardcode-annotations/src/main/java/org/deletethis/hardcode/HardcodeSplit.java
+++ hardcode-annotations/src/main/java/org/deletethis/hardcode/HardcodeSplit.java
@@ -5,8 +5,22 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Marks a long collection or a map, that cannot be constructed in a single function, because of 64k java method size limit.
+ *
+ * <p>It will create multiple methods, each appending certain number of items to the collection. Items must
+ * be referenced just once - by the object, that this annotation is applied to. The collection itself, can be referenced
+ * more than once though.
+ *
+ * <p>Can be applied only on fields.
+ */
 @Retention(RetentionPolicy.RUNTIME)
-@Target({ElementType.TYPE, ElementType.FIELD})
+@Target({ElementType.FIELD})
 public @interface HardcodeSplit {
+    /**
+     * Specifies the number of items, that should be added by a single method. Must be positive.
+     *
+     * @return The number of list/map entries
+     */
     int value();
 }
